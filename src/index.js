1e113b584c70a0a8d0e5f5381c113c3f7a475468
JonasTiago <90968903+JonasTiago@users.noreply.github.com>
2024-09-15 13:45:35 -0300

    initial config and router default

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+import userRoutes from "./routes/userRoutes.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/users", userRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
---
+++ src/routes/userRoutes.js
@@ -0,0 +1,26 @@
+import { Router } from "express";
+import {
+  // createUser,
+  // deleteUser,
+  // findUser,
+  // listUser,
+  // signIn,
+  // updateUser,
+} from "../controllers/userController.js";
+import createUserValidate from "../middlewares/userSchemaMiddleware.js";
+
+const userRoutes = new Router();
+
+// userRoutes.post("/", createUserValidate, createUser);
+
+// userRoutes.get("/", listUser);
+
+// userRoutes.delete("/:user_id", deleteUser);
+
+// userRoutes.get("/:user_id", findUser);
+
+// userRoutes.put("/:user_id", updateUser);
+
+// userRoutes.post("/sign-in", signIn);
+
+export default userRoutes;
